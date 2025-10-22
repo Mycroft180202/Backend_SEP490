5bcb30f4884e9eb5288644d81e9155e95002a70c
Minhbd9 <minhbdhe170083@fpt.edu.vn>
2025-10-22 20:24:58 +0700

    rèace

---
+++ Backend_SEP490/Migrations/20251022105535_AddImageToBlogPost.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_SEP490.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageToBlogPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Backend_SEP490/Migrations/20251022124125_AddEmbeddingToProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_SEP490.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmbeddingToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmbeddingJson",
+                table: "Products",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmbeddingJson",
+                table: "Products");
+        }
+    }
+}
