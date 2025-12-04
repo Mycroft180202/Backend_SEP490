477895e1065f2c01942481254398af83dc0aad2d
Mycroft <mycroft180202@gmail.com>
2025-12-04 12:52:32 +0700

    fix

--- frontend/src/pages/OrderTracking.jsx
+++ frontend/src/pages/OrderTracking.jsx
@@ -1,5 +1,4 @@
-
-import React, { useState } from 'react';
+import React from 'react';
 import Footer from '../components/shared/Footer';
 import Header from '../components/shared/Header';
 import TrackingBar from '../components/orderTracking/TrackingBar';
